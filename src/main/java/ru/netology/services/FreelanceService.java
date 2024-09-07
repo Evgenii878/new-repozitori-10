6650624068ef4983693af30e541d6ce02a7abdeb
Evgenii <miller.zhen@mail.ru>
2024-09-07 19:28:42 +0700

    Initial commit

---
+++ src/main/java/ru/netology/services/FreelanceService.java
@@ -0,0 +1,19 @@
+package ru.netology.services;
+
+public class FreelanceService {
+    public int calculate(int income, int expenses, int treshold) {
+        int maney = 0;
+        int count = 0;
+        for (int i = 0; i < 12; i++) {
+            if (maney >= treshold) {
+                maney -= expenses;
+                maney /= 3;
+                count++;
+            }else {
+                maney += income;
+                maney -= expenses;
+            }
+        }
+        return count;
+    }
+}
